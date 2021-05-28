6bcfd52340e180e5db9c33c7893dc994dd044416
RadioTexnik <84590612+RadioTexnik-rep@users.noreply.github.com>
2021-05-28 21:00:16 +0300

    Delete

--- Blink/Objects/main.d
+++
@@ -1,5 +0,0 @@
-./objects/main.o: Code\src\main.c Code\inc\main.h CMSIS\stm32f3xx.h \
-  CMSIS\stm32f303x8.h CMSIS\core_cm4.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h CMSIS\core_cmInstr.h \
-  CMSIS\cmsis_armcc_V6.h CMSIS\core_cmFunc.h CMSIS\core_cmSimd.h \
-  CMSIS\system_stm32f3xx.h Code\inc\hw_init.h
--- Blink/Objects/system_stm32f30x.d
+++
@@ -1,4 +0,0 @@
-./objects/system_stm32f30x.o: CMSIS\system_stm32f30x.c CMSIS\stm32f30x.h \
-  CMSIS\core_cm4.h C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  CMSIS\core_cmInstr.h CMSIS\core_cmFunc.h CMSIS\core_cmSimd.h \
-  CMSIS\system_stm32f30x.h
